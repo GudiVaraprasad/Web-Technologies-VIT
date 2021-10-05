995c1b96dfe238577f61dd01a1cbae3b83a6308d
Gudi Varaprasad <53944466+GudiVaraprasad@users.noreply.github.com>
2021-10-05 21:43:42 +0530

    Delete firstCharColor.php

--- Theory/WebTech_Assignment/Question1/firstCharColor.php
+++
@@ -1,7 +0,0 @@
-<!-- 1. Write php program, which changes the color of the first character of a word in a sentence. -->
-
-<?php
-$sentence = 'Gudi Varaprasad from Web Technologies class is doing this assignment';
-$sentence = preg_replace('/(\b[a-z])/i','<span style="color:red;">\1</span>',$sentence);
-echo $sentence;
-?>
